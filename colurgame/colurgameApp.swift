088f2b665a5f102a17c7f77283cb290a2190a822
DinansaRithmini <Dinansasoysa@gmail.com>
2026-01-15 23:41:06 +0530

    working game

--- colurgame/colurgameApp.swift
+++ colurgame/colurgameApp.swift
@@ -1,17 +1,11 @@
-//
-//  colurgameApp.swift
-//  colurgame
-//
-//  Created by  Dinansa Rithmini  on 2026-01-13.
-//
-
 import SwiftUI
 
 @main
-struct colurgameApp: App {
+struct SquareGameApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LevelSelectView()   // 👈 FIRST screen
         }
     }
 }
+
